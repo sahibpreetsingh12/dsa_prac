10cc02fcddd3b057c2233118b8746be69672f3f8
sahibpreetsingh12 <ss9334931@gmail.com>
2024-05-05 23:12:16 -0400

    max subarray

--- maxprod.py
+++ maxprod.py
@@ -5,11 +5,11 @@ class Solution:
         for i in range(len(nums)-1):
             if curprod>=0 and nums[i+1]>0:
                 curprod *= nums[i+1]
-                print(f'curprod here is ',curprod)
+                # print(f'curprod here is ',curprod)
             else:
                 curprod = nums[i+1]
             maxprod = max(maxprod,curprod)
-            print(f'Maxprod is ',maxprod)
+            # print(f'Maxprod is ',maxprod)
         return maxprod
 
 sol = Solution()
