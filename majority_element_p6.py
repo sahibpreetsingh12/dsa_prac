2aadfdb2c09dd21d1b1a051b95cc62f84b047115
sahibpreetsingh12 <ss9334931@gmail.com>
2024-05-08 22:04:36 -0400

    majority element with O(n) time complexity

--- majority_element_p6.py
+++ majority_element_p6.py
@@ -53,4 +53,28 @@ print(sol.majorityElement(nums = [3,2,3]))
 """
 Next challenge is to solve in constant time complexity -> O(1)
 
+Boyer-Moore Majority Vote Algorithm
+The algorithm above is known as Boyer-Moore Majority Vote Algorithm.
+The Boyer-Moore Majority Vote Algorithm is an efficient algorithm used to find the majority element (an element that appears more than half of the time) in a given array. 
+This algorithm operates in linear time and requires O(1) additional space.
+
+Here are the basic steps of the Boyer-Moore Majority Vote Algorithm:
+
+  1. Candidate Element Selection: Choose the first element of the array as 
+    the candidate element and initialize a counter.
+
+  2. Element Counting: Traverse through the array. If the current element matches
+        the candidate element, increment the counter. If they don't match, decrement 
+        the counter.
+
+  3. Check Counter: If the counter becomes zero, choose the current element as the 
+        new candidate element and reset the counter to one.
+
+  4. Final Candidate Verification: After these steps, verify if the selected candidate 
+        is indeed the majority element.
+
+By following these steps, the algorithm efficiently identifies the most frequently 
+occurring element, if one exists, in linear time O(n) in the worst case. 
+This efficiency is achieved by carefully selecting candidate elements and comparing them 
+against other elements.
 """
