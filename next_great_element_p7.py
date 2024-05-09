212d3710e87ebb4c529223b57b6286d6dfd0ba00
sahibpreetsingh12 <ss9334931@gmail.com>
2024-05-09 18:02:07 -0400

    max greater element 3 last case pending

--- next_great_element_p7.py
+++ next_great_element_p7.py
@@ -2,9 +2,13 @@ class Solution:
     def nextGreaterElement(self, n: int) -> int:
         nums = str(n)
 
-        if len(set(nums))==1:
+        # if all elemnts are same in the number return -1
+        if len(set(nums))==1: 
             return -1
-        elif list(nums)[0]==max(list(nums)):
+        
+        #if the one's element in the number is the highest in the entire number
+        #than return -1
+        elif list(nums)[0]==max(list(nums)): 
             return -1
 sol = Solution()
 print(sol.nextGreaterElement(11))
