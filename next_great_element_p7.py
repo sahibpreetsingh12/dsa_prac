6cb0d95c1d66ebf47b60f8349683050bfd1f2424
sahibpreetsingh12 <ss9334931@gmail.com>
2024-05-09 21:24:46 -0400

    max greater element 3 last case pending we have to find minimum

--- next_great_element_p7.py
+++ next_great_element_p7.py
@@ -1,3 +1,4 @@
+from itertools import permutations
 class Solution:
     def nextGreaterElement(self, n: int) -> int:
         nums = str(n)
@@ -8,9 +9,22 @@ class Solution:
         
         #if the one's element in the number is the highest in the entire number
         #than return -1
-        elif list(nums)[0]==max(list(nums)): 
+        elif list(nums)[0]==max(list(nums)) and (list(nums)[0]!=list(nums)[-1]): 
             return -1
         else:
-            return int(''.join(sorted(list(nums),reverse=True)))
+            # Get all permutations of the string
+            perms = permutations(nums)
+            # Join each permutation to form strings
+            combinations = map(''.join, perms)
+
+            combinations = sorted([int(i) for i in combinations])
+
+            ele = n
+            for i in combinations:
+                if i>ele:
+                    ele = i
+                    return ele
+            # return list(combinations)
+            # return int(''.join(sorted(list(nums),reverse=True)))
 sol = Solution()
-print(sol.nextGreaterElement(2147483647))
+print(sol.nextGreaterElement(101))
