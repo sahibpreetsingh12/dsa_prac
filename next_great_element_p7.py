203693b5ad2eb5e347edcfcbbb413e0812218263
sahibpreetsingh12 <ss9334931@gmail.com>
2024-05-10 23:02:42 -0400

    greatest elemnt 3 solved but not efficient solving now in 0(n)

--- next_great_element_p7.py
+++ next_great_element_p7.py
@@ -2,15 +2,16 @@ from itertools import permutations
 class Solution:
     def nextGreaterElement(self, n: int) -> int:
         nums = str(n)
+        max_barrier = pow(2, 31) - 1
+        # print(max_barrier)
 
         # if all elemnts are same in the number return -1
         if len(set(nums))==1: 
             return -1
         
-        
         #if the one's element in the number is the highest in the entire number
-        #than return -1
-        elif list(nums)[0]==max(list(nums)) and (list(nums)[0]!=list(nums)[-1]): 
+        # #than return -1
+        elif (list(nums)[0]==max(list(nums))) and (list(nums)[0]!=list(nums)[-1]): 
             return -1
         else:
             # Get all permutations of the string
@@ -24,8 +25,12 @@ class Solution:
             for i in combinations:
                 if i>ele:
                     ele = i
-                    return ele
-            # return list(combinations)
-            # return int(''.join(sorted(list(nums),reverse=True)))
+                    if ele<=max_barrier:
+                        return ele
+                    else:
+                        return -1
+                # else:
+                #     return -1
+
 sol = Solution()
 print(sol.nextGreaterElement(101))
