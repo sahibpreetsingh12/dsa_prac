0030109d045b6b79bdabcfe82b56a0c3a295e750
sahibpreetsingh12 <ss9334931@gmail.com>
2024-05-16 12:42:05 -0400

    comments added

--- next_great_element_p7.py
+++ next_great_element_p7.py
@@ -1,3 +1,26 @@
+"""
+Given a positive integer n, find the smallest integer which has exactly the same 
+digits existing in the integer n and is greater in value than n. If no such positive 
+integer exists, return -1.
+
+Note that the returned integer should fit in 32-bit integer, if there is a valid 
+answer but it does not fit in 32-bit integer, return -1.
+
+ 
+
+Example 1:
+
+Input: n = 12
+Output: 21
+Example 2:
+
+Input: n = 21
+Output: -1
+
+
+"""
+
+
 from itertools import permutations
 class Solution:
     def nextGreaterElement(self, n: int) -> int:
@@ -10,7 +33,9 @@ class Solution:
             return -1
         
         #if the one's element in the number is the highest in the entire number
-        # #than return -1
+        # #than return -1 because think of this carefully if your first digit itself is the bigeest
+        # and last place digit (It can be any Ten's, Hundreds or any) is not same as one' place you
+        # are already dealing with biggest number
 
         elif (list(nums)[0]==max(list(nums))) and (list(nums)[0]!=list(nums)[-1]): 
             return -1
@@ -18,19 +43,26 @@ class Solution:
             # Get all permutations of the string
             perms = permutations(nums)
             # Join each permutation to form strings
+            """
+            Have not taken for loop way because that just increases time complexity
+            """
             combinations = map(''.join, perms)
             combinations = sorted([int(i) for i in combinations])
 
             ele = n
             for i in combinations:
+
+                """
+                if the element in list of combinations has 
+                just exceeded the element (ele) and is les than Barrier (2^31) than return the elemnt
+                """
                 if i>ele:
                     ele = i
                     if ele<=max_barrier:
                         return ele
                     else:
                         return -1
-                # else:
-                #     return -1
+             
 
 sol = Solution()
 print(sol.nextGreaterElement(101))
