df0f80ffdee08471e16c7e51368a0f2a82cd6f29
sahibpreetsingh12 <ss9334931@gmail.com>
2024-05-18 15:42:55 -0400

    max chunks to make sorted

--- max_chunks_to_make_sorted.py
+++ max_chunks_to_make_sorted.py
@@ -20,6 +20,7 @@ Example 2:
 Input: arr = [1,0,2,3,4]
 Output: 4
 Explanation:
+
 We can split into two chunks, such as [1, 0], [2, 3, 4].
 However, splitting into [1, 0], [2], [3], [4] is the highest number of chunks possible.
 
